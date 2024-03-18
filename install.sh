5dd2455a90825e69e5b6ad338802db0fd2a13704
henrique11varela <henrique11varela@gmail.com>
2024-03-18 23:15:07 +0000

    docker ok, desktop by hand

--- install.sh
+++ install.sh
@@ -1,21 +1,54 @@
 #!/bin/sh
 
+#echo 'installing vim'
+echo 'Installing Vim'
 sudo apt install vim
 
+# Install Visual Studio Code
+echo 'Installing vscode'
+wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/863d2581ecda6849923a2118d93a088b0745d9d6/code_1.87.2-1709912201_amd64.deb
+sudo apt install ./code*
+rm ./code*
+
+# Install Brave
+echo 'Installing Brave'
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
 # Install flatpak
-echo 'installing flatpak'
+echo 'Installing flatpak'
 
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+echo 'Installing flatpak apps'
 # Install flatpak apps
-flatpak install flathub com.visualstudio.code
 flatpak install flathub info.febvre.Komikku
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub org.kde.kdenlive
 flatpak install flathub com.spotify.Client
-flatpak install flathub com.brave.Browser
 flatpak install flathub org.videolan.VLC
 flatpak install flathub com.stremio.Stremio
-flatpak install flathub com.github.sdv43.whaler
 
+
+# DOCKER
+echo 'Installing Docker'
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo ''
+echo '<====================================================>'
+echo ''
+echo 'please install Docker-Desktop manually: https://docs.docker.com/desktop/install/ubuntu/'
